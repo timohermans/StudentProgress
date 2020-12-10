f7fb8a9259fd6b99e260c7d942cec295fa8361b7
Timo Hermans <tmhermans@gmail.com>
2020-12-10 21:28:39 +0100

    Make it possible to use Entity Framework in unit tests
    It's getting easier and easier to set this stuff up

---
+++ StudentProgress.Tests/SqliteDbContextTest.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using StudentProgress.Web.Data;
+using StudentProgress.Web.Models;
+using Xunit;
+
+namespace StudentProgress.Tests
+{
+    public class SqliteDbContextTest : DatabaseTests
+    {
+        [Fact]
+        public void DbContext_CreatingASimpleEntity_SuccessfullyInserts()
+        {
+            using var dbContext = new ProgressContext(ContextOptions);
+            dbContext.StudentGroup.Add(new StudentGroup("S3 Leon"));
+            dbContext.SaveChanges();
+
+            var group = dbContext.StudentGroup.FirstOrDefault();
+            
+            Assert.NotNull(group);
+            Assert.NotEqual(0, group.Id);
+        }
+    }
+}
