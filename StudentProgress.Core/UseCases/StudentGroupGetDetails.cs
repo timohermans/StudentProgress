63bda36c5517ae47ebd6893445ccda9ab36dc67c
Timo Hermans <tmhermans@gmail.com>
2020-12-28 22:22:35 +0100

    Fully test the StudentGroupGetDetails.cs
    Very proud of this one :).
    This was broken, but with help of Dapper and integration test fixed!
    
    Thank you, Unit testing book of Vladimir :)

--- StudentProgress.Core/UseCases/StudentGroupGetDetails.cs
+++ StudentProgress.Core/UseCases/StudentGroupGetDetails.cs
@@ -63,33 +63,32 @@ namespace StudentProgress.Core.UseCases
 
         private async Task<Response> GetGroupWithStudentDataOfLatestFeedback(int groupId)
         {
-            // TODO: Change query
             var groupDictionary = new Dictionary<int, Response>();
-            var result = await _connection.QueryAsync<Response, Response.StudentsResponse, Response>(@"
+            var result = await _connection.QueryAsync<Response, Response.StudentsResponse, Response>($@"
 SELECT 
-    g.""Id"", 
+    g.""Id"",
     g.""Name"",
     g.""Mnemonic"",
-    g.""CreatedDate"" as ""CreatedAt"",
-    g.""UpdatedDate"" as ""UpdatedAt"",
+    g.""CreatedDate"" as ""{nameof(Response.CreatedAt)}"",
+    g.""UpdatedDate"" as ""{nameof(Response.UpdatedAt)}"",
     s.""Id"",
     s.""Name"",
-    p2.""Date"" as ""LastUpdateDate"",
-    p2.""ProgressFeeling"" as ""FeelingOfLatestProgress"",
-    p2.""Feedforward"" as ""LastFeedforward""
-FROM ""Group"" g
-LEFT JOIN ""GroupStudent"" gs
-	ON gs.""GroupsId"" = g.""Id""
-LEFT JOIN ""Student"" s
-	ON s.""Id"" = gs.""StudentsId""
-LEFT JOIN ""ProgressUpdate"" p
-	ON p.""StudentId"" = s.""Id""
-LEFT JOIN ""ProgressUpdate"" p2
-	ON p2.""StudentId"" = s.""Id""
-	AND p2.""GroupId"" = g.""Id""
-	AND p2.""Date"" > p.""Date""
-WHERE g.""Id"" = @Id AND p2.""Date"" is not null
-ORDER BY p2.""Date"" DESC;
+    p.""Date"" as ""{nameof(Response.StudentsResponse.LastUpdateDate)}"",
+    p.""ProgressFeeling"" as ""{nameof(Response.StudentsResponse.FeelingOfLatestProgress)}"",
+    p.""Feedforward"" as ""{nameof(Response.StudentsResponse.LastFeedforward)}"",
+	p2.""AmountOfProgressItems"" as ""{nameof(Response.StudentsResponse.AmountOfProgressItems)}""
+FROM ""ProgressUpdate"" p
+INNER JOIN 
+	(select ""StudentId"", MAX(""Date"") as ""Date"", COUNT(*) as ""AmountOfProgressItems""
+	 FROM ""ProgressUpdate"" 
+	 group by ""StudentId"") p2
+	ON p.""StudentId"" = p2.""StudentId""
+INNER JOIN ""Group"" g
+	ON g.""Id"" = p.""GroupId""
+INNER JOIN ""Student"" s
+	ON s.""Id"" = p.""StudentId""
+WHERE p.""Date"" = p2.""Date""
+ORDER BY p.""Date"" DESC
 ", (group, studentProgress) =>
                 {
                     if (!groupDictionary.TryGetValue(group.Id, out var groupEntry))
--- StudentProgress.CoreTests/UseCases/GroupGetDetailsTests.cs
+++ StudentProgress.CoreTests/UseCases/GroupGetDetailsTests.cs
@@ -16,44 +16,63 @@ namespace StudentProgress.CoreTests.UseCases
         }
 
         [Fact]
-        public async Task Gets_the_most_important_details_per_student()
+        public async Task Gets_the_most_important_details_per_student_of_a_group()
         {
+            // arrange
+            Fixture.DataMother.CreateGroup("diversion group", null, "Arnold");
             var group = Fixture.DataMother.CreateGroup("S3 - Leon", "tips", "Timo", "Ryanne");
+            
             var timo = group.Students.FirstOrDefault(g => g.Name == "Timo");
             Fixture.DataMother.CreateProgressUpdate(group, timo,
                 date: new DateTime(2020, 1, 1),
                 feedforward: "work on this 1",
-                feeling: Feeling.Bad
+                feeling: Feeling.Good
             );
             Fixture.DataMother.CreateProgressUpdate(group, timo,
                 date: new DateTime(2020, 2, 2),
                 feedforward: "work on this 2",
                 feeling: Feeling.Bad
             );
+            
             var ryanne = group.Students.FirstOrDefault(g => g.Name == "Ryanne");
             Fixture.DataMother.CreateProgressUpdate(group, ryanne,
                 date: new DateTime(2020, 5, 5),
                 feedforward: "ryanne forward 1",
-                feeling: Feeling.Good
+                feeling: Feeling.Bad
             );
             Fixture.DataMother.CreateProgressUpdate(group, ryanne,
                 date: new DateTime(2020, 6, 6),
                 feedforward: "ryanne forward 2",
                 feeling: Feeling.Good
             );
+            
             using var ucConnection = Fixture.CreateDbConnection();
             var useCase = new StudentGroupGetDetails(ucConnection);
 
+            // act
             var response = await useCase.HandleAsync(new StudentGroupGetDetails.Request(group.Id));
 
+            // assert
             response.Should().NotBeNull();
             response!.Id.Should().Be(group.Id);
             response!.Mnemonic.Should().Be("tips");
             response!.Name.Should().Be("S3 - Leon");
-            response!.Students.Count().Should().Be(2);
+            
+            response!.Students.Count.Should().Be(2);
+            
+            response!.Students.ElementAt(0).Id.Should().Be(ryanne!.Id);
             response!.Students.ElementAt(0).Name.Should().Be("Ryanne");
+            response!.Students.ElementAt(0).LastUpdateDate.Should().Be(new DateTime(2020, 6, 6));
+            response!.Students.ElementAt(0).LastFeedforward.Should().Be("ryanne forward 2");
+            response!.Students.ElementAt(0).AmountOfProgressItems.Should().Be(2);
+            response!.Students.ElementAt(0).FeelingOfLatestProgress.Should().Be(Feeling.Good);
+            
+            response!.Students.ElementAt(1).Id.Should().Be(timo!.Id);
             response!.Students.ElementAt(1).Name.Should().Be("Timo");
-            // TODO: Assert all the things
+            response!.Students.ElementAt(1).LastUpdateDate.Should().Be(new DateTime(2020, 2, 2));
+            response!.Students.ElementAt(1).LastFeedforward.Should().Be("work on this 2");
+            response!.Students.ElementAt(1).AmountOfProgressItems.Should().Be(2);
+            response!.Students.ElementAt(1).FeelingOfLatestProgress.Should().Be(Feeling.Bad);
         }
     }
 }
