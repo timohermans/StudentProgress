8edf79703f21b06d3cffd9ea35fdab9aef0cef1d
Timo Hermans <tmhermans@gmail.com>
2021-01-05 22:08:31 +0100

    Create design for progress milestones in student progress

--- StudentProgress.Web/Pages/Progress/Index.cshtml.cs
+++ StudentProgress.Web/Pages/Progress/Index.cshtml.cs
@@ -2,10 +2,34 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StudentProgress.Core.Entities;
 using StudentProgress.Core.UseCases;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace StudentProgress.Web.Pages.Progress
 {
+    public enum Rating
+    {
+        Undefined = 1,
+        Orienting = 2,
+        Beginning = 3,
+        Proficient = 4,
+        Advanced = 5
+    }
+
+    public class ProgressMilestone
+    {
+        public string LearningOutcome { get; }
+        public string Artifact { get; }
+        public Rating CurrentRating { get; }
+
+        public ProgressMilestone(string learningOutcome, string artifact, Rating rating)
+        {
+            LearningOutcome = learningOutcome ?? throw new NullReferenceException(nameof(learningOutcome));
+            Artifact = artifact ?? throw new NullReferenceException(nameof(artifact));
+            CurrentRating = rating;
+        }
+    }
     public class IndexModel : PageModel
     {
         private readonly ProgressGetForStudentInGroup _useCase;
@@ -14,6 +38,28 @@ namespace StudentProgress.Web.Pages.Progress
         {
             _useCase = new ProgressGetForStudentInGroup(context);
         }
+
+
+        // styling dummy code
+        public List<ProgressMilestone> ProgressMilestones = new List<ProgressMilestone>
+{
+    //new ProgressMilestone("1. Feedback van stakeholders", "Compleetheid documentatie", Rating.Proficient),
+    //new ProgressMilestone("1. Feedback van stakeholders", "Onderbouwing beslissingen", Rating.Proficient),
+    //new ProgressMilestone("2. Samenwerking en communicatie", "Samenwerking/communicatie", Rating.Orienting),
+    //new ProgressMilestone("2. Samenwerking en communicatie", "Documentatie/meetings van waarde voor stakeholder", Rating.Orienting),
+    //new ProgressMilestone("2. Samenwerking en communicatie", "Regelmatig vragen feedback", Rating.Orienting),
+    new ProgressMilestone("3. Specificaties en ontwerpen", "Analysedocument", Rating.Proficient),
+    //new ProgressMilestone("3. Specificaties en ontwerpen", "Acceptatie testen of integratie testen", Rating.Orienting),
+    new ProgressMilestone("3. Specificaties en ontwerpen", "Ontwerpdocument", Rating.Orienting),
+    new ProgressMilestone("4. Onderhoudbare OO applicaties", "Herhaaldelijk feedback", Rating.Advanced),
+    //new ProgressMilestone("4. Onderhoudbare OO applicaties", "Up to date houden ontwerpdocument", Rating.Orienting),
+    new ProgressMilestone("4. Onderhoudbare OO applicaties", "Deployment", Rating.Undefined),
+    //new ProgressMilestone("4. Onderhoudbare OO applicaties", "Error handling", Rating.Orienting),
+    //new ProgressMilestone("4. Onderhoudbare OO applicaties", "Onderhoudbaar (met of zonder docs)", Rating.Orienting),
+    new ProgressMilestone("4. Onderhoudbare OO applicaties", "OO principes", Rating.Beginning),
+    //new ProgressMilestone("5. Algoritmiek", "Circustrein", Rating.Orienting),
+    //new ProgressMilestone("5. Algoritmiek", "Containerschip", Rating.Orienting)
+};
 
         public ProgressGetForStudentInGroup.Response Student { get; set; }
 
