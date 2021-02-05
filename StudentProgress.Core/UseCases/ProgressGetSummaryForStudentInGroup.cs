ec16692491f9f122b9ef0b7bff18d1ecbcd35fce
Timo Hermans <tmhermans@gmail.com>
2021-02-05 09:29:42 +0100

    Add timeline to summary

--- StudentProgress.Core/UseCases/ProgressGetSummaryForStudentInGroup.cs
+++ StudentProgress.Core/UseCases/ProgressGetSummaryForStudentInGroup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,99 +10,117 @@ using StudentProgress.Core.Entities;
 
 namespace StudentProgress.Core.UseCases
 {
-  public class ProgressGetSummaryForStudentInGroup
-  {
-    public record Query
+    public class ProgressGetSummaryForStudentInGroup
     {
-      public int GroupId { get; set; }
-      public int StudentId { get; set; }
+        public record Query
+        {
+            public int GroupId { get; set; }
+            public int StudentId { get; set; }
+        }
+
+        public record MilestoneResponse
+        {
+            public string Artefact { get; set; }
+            public string LearningOutcome { get; set; }
+            public Rating? Rating { get; set; }
+            public string? Comment { get; set; }
+            public int TimesWorkedOn { get; set; }
+
+            public MilestoneResponse(string artefact, string learningOutcome, Rating? rating, string? comment,
+                int timesWorkedOn)
+            {
+                Artefact = artefact;
+                LearningOutcome = learningOutcome;
+                Rating = rating;
+                Comment = comment;
+                TimesWorkedOn = timesWorkedOn;
+            }
+        }
+
+        public record ProgressUpdateResponse(int Id, DateTime Date, Feeling Feeling, int StudentId, int GroupId);
+
+        public record Response
+        {
+            public int GroupId { get; set; }
+            public int StudentId { get; set; }
+            public string GroupName { get; set; }
+            public string StudentName { get; set; }
+            public Period Period { get; set; }
+            public IEnumerable<MilestoneResponse> Milestones { get; set; }
+            public IEnumerable<ProgressUpdateResponse> ProgressUpdates { get; set; }
+
+            public Response(int groupId, int studentId, string groupName, string studentName, Period period,
+                IEnumerable<MilestoneResponse> milestones, IEnumerable<ProgressUpdateResponse> progressUpdates)
+            {
+                GroupId = groupId;
+                StudentId = studentId;
+                GroupName = groupName;
+                StudentName = studentName;
+                Period = period;
+                Milestones = milestones;
+                ProgressUpdates = progressUpdates;
+            }
+        }
+
+        private readonly ProgressContext _context;
+
+        public ProgressGetSummaryForStudentInGroup(ProgressContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<Response>> HandleAsync(Query query)
+        {
+            var group = Maybe<StudentGroup>.From(await _context.Groups.FindAsync(query.GroupId))
+                .ToResult("Group does not exist");
+            var student = Maybe<Student>.From(await _context.Students.FindAsync(query.StudentId))
+                .ToResult("Student does not exist");
+            var doGroupStudentExist = Result.Combine(group, student);
+
+            if (doGroupStudentExist.IsFailure)
+            {
+                return Result.Failure<Response>(doGroupStudentExist.Error);
+            }
+
+            var milestones = await _context.Milestones.Where(m => m.StudentGroup.Id == query.GroupId).ToListAsync();
+            var studentMilestoneProgresses = await _context.MilestoneProgresses
+                .Include(mp => mp.Milestone)
+                .Where(mp =>
+                    mp.ProgressUpdate.StudentId == query.StudentId && mp.Milestone.StudentGroup.Id == query.GroupId)
+                .OrderByDescending(mp => mp.ProgressUpdate.Date)
+                .ToListAsync();
+            var progressUpdates = await _context.ProgressUpdates
+                .Where(u => u.GroupId == query.GroupId && u.StudentId == query.StudentId)
+                .Select(u => new ProgressUpdateResponse(u.Id, u.Date, u.ProgressFeeling, u.StudentId, u.GroupId))
+                .ToListAsync();
+
+            var milestonesSummary = milestones.Select(milestone =>
+                {
+                    var milestoneProgresses =
+                        studentMilestoneProgresses.Where(mp => mp.Milestone.Id == milestone.Id).ToList();
+                    var latestProgress = milestoneProgresses.FirstOrDefault();
+
+                    return new MilestoneResponse(
+                        artefact: milestone.Artefact,
+                        learningOutcome: milestone.LearningOutcome,
+                        rating: latestProgress?.Rating,
+                        comment: latestProgress?.Comment,
+                        timesWorkedOn: milestoneProgresses.Count
+                    );
+                })
+                .OrderBy(m => m.LearningOutcome)
+                .ThenBy(m => m.Artefact)
+                .ToList();
+
+            return Result.Success(new Response(
+                groupId: group.Value.Id,
+                groupName: group.Value.Name,
+                studentId: student.Value.Id,
+                studentName: student.Value.Name,
+                milestones: milestonesSummary,
+                progressUpdates: progressUpdates,
+                period: group.Value.Period
+            ));
+        }
     }
-
-    public record MilestoneResponse
-    {
-      public string Artefact { get; set; }
-      public string LearningOutcome { get; set; }
-      public Rating? Rating { get; set; }
-      public string? Comment { get; set; }
-      public int TimesWorkedOn { get; set; }
-
-      public MilestoneResponse(string artefact, string learningOutcome, Rating? rating, string? comment, int timesWorkedOn)
-      {
-        Artefact = artefact;
-        LearningOutcome = learningOutcome;
-        Rating = rating;
-        Comment = comment;
-        TimesWorkedOn = timesWorkedOn;
-      }
-    }
-
-    public record Response
-    {
-      public int GroupId { get; set; }
-      public int StudentId { get; set; }
-      public string GroupName { get; set; }
-      public string StudentName { get; set; }
-      public IEnumerable<MilestoneResponse> Milestones { get; set; }
-
-      public Response(int groupId, int studentId, string groupName, string studentName, IEnumerable<MilestoneResponse> milestones)
-      {
-        GroupId = groupId;
-        StudentId = studentId;
-        GroupName = groupName;
-        StudentName = studentName;
-        Milestones = milestones;
-      }
-    }
-
-    private readonly ProgressContext _context;
-
-    public ProgressGetSummaryForStudentInGroup(ProgressContext context)
-    {
-      _context = context;
-    }
-
-    public async Task<Result<Response>> HandleAsync(Query query)
-    {
-      var group = Maybe<StudentGroup>.From(await _context.Groups.FindAsync(query.GroupId)).ToResult("Group does not exist");
-      var student = Maybe<Student>.From(await _context.Students.FindAsync(query.StudentId)).ToResult("Student does not exist");
-      var doGroupStudentExist = Result.Combine(group, student);
-
-      if (doGroupStudentExist.IsFailure)
-      {
-        return Result.Failure<Response>(doGroupStudentExist.Error);
-      }
-
-      var milestones = await _context.Milestones.Where(m => m.StudentGroup.Id == query.GroupId).ToListAsync();
-      var studentProgresses = await _context.MilestoneProgresses
-                                .Include(mp => mp.Milestone)
-                                .Where(mp => mp.ProgressUpdate.StudentId == query.StudentId && mp.Milestone.StudentGroup.Id == query.GroupId)
-                                .OrderByDescending(mp => mp.ProgressUpdate.Date)
-                                .ToListAsync();
-
-      var milestonesSummary = milestones.Select(milestone =>
-      {
-        var milestoneProgresses = studentProgresses.Where(mp => mp.Milestone.Id == milestone.Id).ToList();
-        var latestProgress = milestoneProgresses.FirstOrDefault();
-
-        return new MilestoneResponse(
-          artefact: milestone.Artefact,
-          learningOutcome: milestone.LearningOutcome,
-          rating: latestProgress?.Rating,
-          comment: latestProgress?.Comment,
-          timesWorkedOn: milestoneProgresses.Count
-        );
-      })
-      .OrderBy(m => m.LearningOutcome)
-      .ThenBy(m => m.Artefact)
-      .ToList();
-
-      return Result.Success(new Response(
-        groupId: group.Value.Id,
-        groupName: group.Value.Name,
-        studentId: student.Value.Id,
-        studentName: student.Value.Name,
-        milestones: milestonesSummary
-      ));
-    }
-  }
 }
