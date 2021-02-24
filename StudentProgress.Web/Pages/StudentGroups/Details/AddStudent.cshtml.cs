3061a17ac4a6df6a93b987bb1d6f7e82d6b09165
Timo Hermans <tmhermans@gmail.com>
2021-02-24 11:37:11 +0100

    Refactor some C# goodness

--- StudentProgress.Web/Pages/StudentGroups/Details/AddStudent.cshtml.cs
+++ StudentProgress.Web/Pages/StudentGroups/Details/AddStudent.cshtml.cs
@@ -8,70 +8,70 @@ using System.Threading.Tasks;
 
 namespace StudentProgress.Web.Pages.StudentGroups.Details
 {
-    public class AddStudentModel : PageModel
+  public class AddStudentModel : PageModel
+  {
+    private readonly ProgressContext _context;
+    private readonly StudentAddToGroup _useCase;
+
+    public StudentGroup Group { get; set; }
+
+    public AddStudentModel(ProgressContext context)
     {
-        private readonly ProgressContext _context;
-        private readonly StudentAddToGroup _useCase;
-
-        public StudentGroup Group { get; set; }
-
-        public AddStudentModel(ProgressContext context)
-        {
-            _context = context;
-            _useCase = new StudentAddToGroup(_context);
-        }
-
-        public IActionResult OnGet(int? groupId)
-        {
-            Group = _context.Groups.FirstOrDefault(g => g.Id == (groupId ?? 0));
-            if (Group == null)
-            {
-                RedirectToPage("/StudentGroups/Index");
-            }
-
-            return Page();
-        }
-
-        public int? GroupId { get; set; }
-
-        [BindProperty] public StudentAddToGroup.Request Student { get; set; }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            await AddStudentToGroup();
-
-            if (!ModelState.IsValid) return Page();
-
-            return RedirectToPage("./Index", new {Id = Student.GroupId});
-        }
-
-        public async Task<IActionResult> OnPostAndAddAnotherAsync()
-        {
-            await AddStudentToGroup();
-
-            if (!ModelState.IsValid) return Page();
-
-            return RedirectToPage("./AddStudent", new {GroupId = Student.GroupId});
-        }
-
-        private async Task<IActionResult> AddStudentToGroup()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            try
-            {
-                await _useCase.HandleAsync(Student);
-            }
-            catch (InvalidOperationException ex)
-            {
-                ModelState.AddModelError("Summary", ex.Message);
-                return Page();
-            }
-
-            return Page();
-        }
+      _context = context;
+      _useCase = new StudentAddToGroup(_context);
     }
+
+    public IActionResult OnGet(int? groupId)
+    {
+      Group = _context.Groups.FirstOrDefault(g => g.Id == (groupId ?? 0));
+      if (Group == null)
+      {
+        RedirectToPage("/StudentGroups/Index");
+      }
+
+      return Page();
+    }
+
+    public int? GroupId { get; set; }
+
+    [BindProperty] public StudentAddToGroup.Request Student { get; set; }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+      await AddStudentToGroup();
+
+      if (!ModelState.IsValid) return Page();
+
+      return RedirectToPage("./Index", new { Id = Student.GroupId });
+    }
+
+    public async Task<IActionResult> OnPostAndAddAnotherAsync()
+    {
+      await AddStudentToGroup();
+
+      if (!ModelState.IsValid) return Page();
+
+      return RedirectToPage("./AddStudent", new { Student.GroupId });
+    }
+
+    private async Task<IActionResult> AddStudentToGroup()
+    {
+      if (!ModelState.IsValid)
+      {
+        return Page();
+      }
+
+      try
+      {
+        await _useCase.HandleAsync(Student);
+      }
+      catch (InvalidOperationException ex)
+      {
+        ModelState.AddModelError("Summary", ex.Message);
+        return Page();
+      }
+
+      return Page();
+    }
+  }
 }
