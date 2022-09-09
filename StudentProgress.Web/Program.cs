78104eb0142afb9471d37d306b72023685824c8c
Timo Hermans <tmhermans@gmail.com>
2022-09-09 17:10:47 +0200

    Remove all authentication possibilities and use new single startup point

--- StudentProgress.Web/Program.cs
+++ StudentProgress.Web/Program.cs
@@ -1,20 +1,50 @@
-using Microsoft.AspNetCore.Hosting;
+using HtmlTags;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StudentProgress.Core.Entities;
+using StudentProgress.Web.Infrastructure;
 
-namespace StudentProgress.Web
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddMiniProfiler().AddEntityFramework();
+builder.Services.AddRazorPages();
+builder.Services.AddHtmlTags(new TagConventions());
+builder.Services.AddDbContext<ProgressContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("ProgressContext"),
+        b => b.MigrationsAssembly("StudentProgress.Core")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+else
+{
+    app.UseMiniProfiler();
+    app.UseBrowserLink();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapRazorPages();
+
+app.Run();
+
+using var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+var context = serviceScope.ServiceProvider.GetService<ProgressContext>();
+context?.Database.Migrate();
