688d463992af82b5e52b3bac093467e47ff86e95
Timo Hermans <tmhermans@gmail.com>
2022-09-14 11:51:53 +0200

    Assign feeling by shortcut (shift+1, etc)

---
+++ StudentProgress.Web/wwwroot/js/progressCreateEdit.js
@@ -0,0 +1,18 @@
+﻿function hotkeyController() {
+    return {
+        message: "hello folks!",
+        listenToKeys() {
+            document.onkeyup = (e) => {
+                if (['input', 'textarea'].includes(document.activeElement.tagName.toLowerCase())) return;
+                
+                const feelingKeys = ["!", "@", "#"];
+                if (feelingKeys.includes(e.key)) {
+                    const keySelected = (feelingKeys.indexOf(e.key) + 1).toString();
+                    const option = [...this.$refs.feelingSelect.options].find(o => o.value === keySelected);
+                    if (option) this.$refs.feelingSelect.value = option.value;
+                }
+                
+            };
+        }
+    };
+}
