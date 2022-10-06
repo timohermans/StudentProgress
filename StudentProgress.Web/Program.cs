602d11346a684540279502ea6b7d2b638e1b03e2
Timo Hermans <tmhermans@gmail.com>
2022-10-06 22:21:56 +0200

    Try enable https via hacky way

--- StudentProgress.Web/Program.cs
+++ StudentProgress.Web/Program.cs
@@ -62,6 +62,11 @@ app.UseForwardedHeaders();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.Use((context, next) =>
+    {
+        context.Request.Scheme = "https";
+        return next();
+    });
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
