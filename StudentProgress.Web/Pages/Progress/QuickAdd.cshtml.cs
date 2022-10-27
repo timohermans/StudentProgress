1f321496b638dcfba77f14e4a4dc449de9cc0107
Timo Hermans <tmhermans@gmail.com>
2022-10-27 20:17:59 +0200

    Finish quick add functionality

--- StudentProgress.Web/Pages/Progress/QuickAdd.cshtml.cs
+++ StudentProgress.Web/Pages/Progress/QuickAdd.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,17 +17,18 @@ public class QuickAdd : PageModel
 {
     private readonly ProgressContext _db;
 
-    [BindProperty] public QuickAddCommand Command { get; set; } = null!;
+    [BindProperty] public QuickAddCommand? Command { get; set; }
     public IList<SelectListItem> Groups { get; set; } = null!;
 
     public StudentGroup? Group { get; set; }
+    public List<SelectListItem> Milestones { get; set; } = new();
 
     public QuickAdd(ProgressContext db)
     {
         _db = db;
     }
 
-    public async Task OnGet(int? groupId)
+    public async Task<IActionResult> OnGet(int? groupId)
     {
         var groups = await _db
             .Groups
@@ -36,7 +38,7 @@ public class QuickAdd : PageModel
         Groups = groups
             .Select(g => new SelectListItem(g.Name, g.Id.ToString(), g.Id == groupId))
             .ToList();
-        Groups.Insert(0, new SelectListItem("", null, groupId == null));
+        Groups.Insert(0, new SelectListItem("-- Select a group --", null, groupId == null));
 
         if (groupId.HasValue)
         {
@@ -46,12 +48,73 @@ public class QuickAdd : PageModel
                 .Include(g => g.Students)
                 .ThenInclude(s => s.ProgressUpdates)
                 .FirstAsync(g => g.Id == groupId);
-            Command = new QuickAddCommand
+            Milestones = Group
+                .Milestones
+                .OrderBy(m => m.LearningOutcome)
+                .ThenBy(m => m.Artefact)
+                .Select(m => new SelectListItem(m.ToString(), m.Id.ToString()))
+                .ToList();
+            Milestones.Insert(0, new SelectListItem("-- Select a milestone --", null));
+
+            Command ??= new QuickAddCommand
             {
                 GroupId = groupId.Value,
-                Students = Group.Students.Select(s => new QuickAddCommand.StudentCommand(s.Id, null)).ToList()
+                Students = Group.Students.Select(s => new QuickAddCommand.StudentCommand(s.Id, null, null)).ToList()
             };
         }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int? groupId)
+    {
+        if (!ModelState.IsValid || !groupId.HasValue)
+        {
+            return await OnGet(groupId);
+        }
+
+        var group = await _db
+            .Groups
+            .Include(g => g.Students)
+            .Include(g => g.Milestones)
+            .FirstOrDefaultAsync(g => g.Id == groupId);
+
+        if (group == null || Command == null)
+        {
+            return NotFound();
+        }
+
+        var progressUpdatesToCreate = Command
+            .Students
+            .Where(s => s.Rating.HasValue || !string.IsNullOrWhiteSpace(s.Comment))
+            .Select(s =>
+            {
+                var student = group.Students.First(dbStudent => dbStudent.Id == s.StudentId);
+                var milestone = group.Milestones.First(m => m.Id == Command.MilestoneId);
+                var rating = s.Rating ?? Rating.Orienting; 
+                var progressUpdate = new ProgressUpdate(
+                    student,
+                    group,
+                    null,
+                    (int)rating switch
+                    {
+                        < 3 => Feeling.Bad,
+                        3 => Feeling.Neutral,
+                        > 3 => Feeling.Good
+                    },
+                    DateTime.Now, 
+                    true);
+
+                progressUpdate.AddMilestones(new[] { new MilestoneProgress(rating, milestone, s.Comment) });
+                return progressUpdate;
+            })
+            .ToList();
+
+        await _db.ProgressUpdates.AddRangeAsync(progressUpdatesToCreate);
+        await _db.SaveChangesAsync();
+
+        return RedirectToPage("/StudentGroups/Details/Index", new { Id = Command.GroupId, Sort = "last-feedback" });
+        // TODO: unit test this
     }
 
     public class QuickAddCommand : IUseCaseRequest<Result>
@@ -60,6 +123,6 @@ public class QuickAdd : PageModel
         [Required] public int MilestoneId { get; init; }
         public List<StudentCommand> Students { get; init; } = new();
 
-        public record StudentCommand(int StudentId, Rating? Feeling);
+        public record StudentCommand(int StudentId, Rating? Rating, string? Comment);
     }
 }
