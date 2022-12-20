4ff8695cc55fd27c93069b58d730222b662cd3a3
Timo Hermans <tmhermans@gmail.com>
2022-12-20 21:19:30 +0100

    Start styling the groups page with Every Layout and sass
    HTML and css is becoming way way simpler <3

--- StudentProgress.Web/Client/scripts/site.ts
+++ StudentProgress.Web/Client/scripts/site.ts
@@ -1,4 +1,11 @@
 ﻿import BaBarChart from "bootstrap-icons/icons/bar-chart-steps.svg?raw";
+import QuickAddIcon from "bootstrap-icons/icons/clipboard2-plus.svg?raw";
+import GroupsIcon  from "bootstrap-icons/icons/person-lines-fill.svg?raw";
+import SettingsIcon from "bootstrap-icons/icons/gear.svg?raw";
 
-(document.querySelector('#brandImg') as HTMLDivElement).innerHTML = BaBarChart;
+
+(document.querySelector('#brandSvg') as HTMLDivElement).innerHTML = BaBarChart;
+(document.querySelector('#quickAddSvg') as HTMLDivElement).innerHTML = QuickAddIcon;
+(document.querySelector('#groupsSvg') as HTMLDivElement).innerHTML = GroupsIcon;
+(document.querySelector('#settingsSvg') as HTMLDivElement).innerHTML = SettingsIcon;
 console.log('hello world');
