63c594d9353a0acfb70dc366290f83d20a6d038a
Timo Hermans <tmhermans@gmail.com>
2022-12-20 16:57:55 +0100

    Remove usage of css frameworks and add proper sass support out of profile
    Maybe I'll add bootstrap later, but don't know yet if I'll need it

--- StudentProgress.Web/Client/scripts/site.ts
+++ StudentProgress.Web/Client/scripts/site.ts
@@ -1,1 +1,4 @@
-﻿console.log('hello world');
+﻿import BaBarChart from "bootstrap-icons/icons/bar-chart-steps.svg?raw";
+
+(document.querySelector('#brandImg') as HTMLDivElement).innerHTML = BaBarChart;
+console.log('hello world');
