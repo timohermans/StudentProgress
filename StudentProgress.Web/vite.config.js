3077ba1d5137d51ad8814d73674ba5d8cbbf0b84
Timo Hermans <tmhermans@gmail.com>
2023-01-04 19:43:20 +0100

    Migrate to Tailwind css

--- StudentProgress.Web/vite.config.js
+++ StudentProgress.Web/vite.config.js
@@ -1,23 +1,25 @@
-import { defineConfig } from 'vite';
-import glob from 'glob';
-import mkcert from 'vite-plugin-mkcert'
+import { defineConfig } from "vite";
+import glob from "glob";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-    server: {
-        https: true,
-        cors: true,
+  server: {
+    https: true
+  },
+  plugins: [mkcert()],
+  build: {
+    manifest: true,
+    outDir: "wwwroot/scripts",
+    assetsDir: "",
+    sourcemap: true,
+    modulePreload: {
+      polyfill: false,
     },
-    plugins: [mkcert()],
-    build: {
-        manifest: true,
-        outDir: 'wwwroot/scripts',
-        assetsDir: '',
-        sourcemap: true,
-        modulePreload: {
-            polyfill: false
-        },
-        rollupOptions: {
-            input: [...glob.sync('Client/**/*.ts'), ...glob.sync('Client/**/!(_)*.scss')]
-        }
-    }
-})
+    rollupOptions: {
+      input: [
+        ...glob.sync("Client/**/*.ts"),
+        ...glob.sync("Client/**/!(_)*.scss"),
+      ],
+    },
+  },
+});
