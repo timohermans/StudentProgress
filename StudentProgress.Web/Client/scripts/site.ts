b11a00ccfc6eda73afbbad04b4c218b87cbfbdec
Timo Hermans <tmhermans@gmail.com>
2023-01-04 15:42:09 +0100

    Work on styling

--- StudentProgress.Web/Client/scripts/site.ts
+++ StudentProgress.Web/Client/scripts/site.ts
@@ -2,10 +2,11 @@
 import QuickAddIcon from "bootstrap-icons/icons/clipboard2-plus.svg?raw";
 import GroupsIcon  from "bootstrap-icons/icons/person-lines-fill.svg?raw";
 import SettingsIcon from "bootstrap-icons/icons/gear.svg?raw";
+import LogoutIcon from "bootstrap-icons/icons/door-open.svg?raw";
 
 
 (document.querySelector('#brandSvg') as HTMLDivElement).innerHTML = BaBarChart;
 (document.querySelector('#quickAddSvg') as HTMLDivElement).innerHTML = QuickAddIcon;
 (document.querySelector('#groupsSvg') as HTMLDivElement).innerHTML = GroupsIcon;
 (document.querySelector('#settingsSvg') as HTMLDivElement).innerHTML = SettingsIcon;
-console.log('hello world');
+(document.querySelector('#logoutSvg') as HTMLDivElement).innerHTML = LogoutIcon;
