2b6ddfb5269736ed92117b8b82d6fc7f24e3dcd4
Timo Hermans <tmhermans@gmail.com>
2023-11-22 19:45:35 +0100

    Add migrations for quest and questline changes

---
+++ StudentProgress.Web/Migrations/20231121065507_AddNameToQuest.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentProgress.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameToQuest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Objective",
+                newName: "Name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ObjectiveMain",
+                table: "Quest",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Quest",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Quest",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Quest");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Objective",
+                newName: "Title");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ObjectiveMain",
+                table: "Quest",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Quest",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
---
+++ StudentProgress.Web/Migrations/20231122070833_RenameObjectiveInQuest.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentProgress.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameObjectiveInQuest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Objective_Quest_QuestId",
+                table: "Objective");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Quest_QuestLines_QuestLineId",
+                table: "Quest");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Quest",
+                table: "Quest");
+
+            migrationBuilder.DropColumn(
+                name: "MainObjective",
+                table: "QuestLines");
+
+            migrationBuilder.RenameTable(
+                name: "Quest",
+                newName: "Quests");
+
+            migrationBuilder.RenameColumn(
+                name: "ObjectiveMain",
+                table: "Quests",
+                newName: "MainObjective");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Quest_QuestLineId",
+                table: "Quests",
+                newName: "IX_Quests_QuestLineId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Quests",
+                table: "Quests",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Objective_Quests_QuestId",
+                table: "Objective",
+                column: "QuestId",
+                principalTable: "Quests",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Quests_QuestLines_QuestLineId",
+                table: "Quests",
+                column: "QuestLineId",
+                principalTable: "QuestLines",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Objective_Quests_QuestId",
+                table: "Objective");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Quests_QuestLines_QuestLineId",
+                table: "Quests");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Quests",
+                table: "Quests");
+
+            migrationBuilder.RenameTable(
+                name: "Quests",
+                newName: "Quest");
+
+            migrationBuilder.RenameColumn(
+                name: "MainObjective",
+                table: "Quest",
+                newName: "ObjectiveMain");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Quests_QuestLineId",
+                table: "Quest",
+                newName: "IX_Quest_QuestLineId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MainObjective",
+                table: "QuestLines",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Quest",
+                table: "Quest",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Objective_Quest_QuestId",
+                table: "Objective",
+                column: "QuestId",
+                principalTable: "Quest",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Quest_QuestLines_QuestLineId",
+                table: "Quest",
+                column: "QuestLineId",
+                principalTable: "QuestLines",
+                principalColumn: "Id");
+        }
+    }
+}
