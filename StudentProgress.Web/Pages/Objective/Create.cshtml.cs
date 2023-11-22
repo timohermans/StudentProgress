63d5df99da3ef2afb6fabeb4ee5b8fdca6c6b4b5
Timo Hermans <tmhermans@gmail.com>
2023-11-22 19:45:44 +0100

    Start working on objectives

---
+++ StudentProgress.Web/Pages/Objective/Create.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentProgress.Web.Pages.Objective;
+
+public class Create : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
